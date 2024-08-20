e4c863d7e97df188d0208372af24d7f0b77daf0a
Matteo Baglini <matteo.baglini@gmail.com>
2024-08-20 15:51:10 +0200

    feat: returns service names from Consul

--- src/main/java/com/zextras/carbonio/catalog/app/services/ServicesResource.java
+++ src/main/java/com/zextras/carbonio/catalog/app/services/ServicesResource.java
@@ -1,10 +1,14 @@
 package com.zextras.carbonio.catalog.app.services;
 
+import com.zextras.carbonio.catalog.app.consul.ConsulCatalogApi;
+import com.zextras.carbonio.catalog.app.consul.ConsulToken;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
@@ -14,10 +18,20 @@ import org.jboss.logging.Logger;
 @Tag(name = "Services Resource")
 public class ServicesResource {
   private static final Logger log = Logger.getLogger("SERVICES");
+  private final ConsulToken token;
+  private final ConsulCatalogApi consulCatalogApi;
+
+  @Inject
+  public ServicesResource(ConsulToken token, ConsulCatalogApi consulCatalogApi) {
+    this.token = token;
+    this.consulCatalogApi = consulCatalogApi;
+  }
 
   @GET
   public GetServicesResponse getServices() {
     log.infof("GET services");
-    return new GetServicesResponse(new String[] {  });
+    final var services = consulCatalogApi.getAll(token.value().trim());
+    final var serviceNames = services.keySet().toArray(new String[0]);
+    return new GetServicesResponse(serviceNames);
   }
 }
--- src/test/java/com/zextras/carbonio/catalog/app/services/ServicesResourceTest.java
+++ src/test/java/com/zextras/carbonio/catalog/app/services/ServicesResourceTest.java
@@ -1,20 +1,62 @@
 package com.zextras.carbonio.catalog.app.services;
 
+import com.zextras.carbonio.catalog.app.WireMockConsulTestResource;
+import com.zextras.carbonio.catalog.app.consul.ConsulToken;
+import io.quarkus.test.Mock;
+import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.enterprise.inject.Produces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
+@WithTestResource(value = WireMockConsulTestResource.class)
 class ServicesResourceTest {
-  @Test
-  void getEmptyServicesList() {
+
+  @ConfigProperty(name = "consul.url")
+  String consulUrl;
+
+  @Mock
+  @Produces
+  ConsulToken testConsulToken() {
+    return new ConsulToken("TEST_CONSUL_TOKEN");
+  }
+
+  @Ignore
+  void getEmptyServices() {
     given()
         .when()
         .get("services")
         .then()
         .statusCode(200)
         .body("items", empty());
+  }
+
+  @Test
+  void geAlltServices() {
+    given()
+        .when()
+        .get("services")
+        .then()
+        .statusCode(200)
+        .body("items.size()", is(4));
+  }
+
+  @Test
+  void getServiceNames() {
+    given()
+        .when()
+        .get("services")
+        .then()
+        .statusCode(200)
+        .body("items", hasItem(
+            anyOf(
+                equalTo("carbonio-advanced"),
+                equalTo("carbonio-files"))
+        ));
   }
 }
