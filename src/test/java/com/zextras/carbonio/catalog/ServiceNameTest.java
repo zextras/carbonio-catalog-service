960ad7fcaf63907b144c769127e3b9df0718bf38
Matteo Baglini <matteo.baglini@gmail.com>
2024-08-26 10:39:18 +0200

    refactor: use parameterized tests for exclude unwanted services

--- src/test/java/com/zextras/carbonio/catalog/ServiceNameTest.java
+++ src/test/java/com/zextras/carbonio/catalog/ServiceNameTest.java
@@ -1,6 +1,8 @@
 package com.zextras.carbonio.catalog;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Set;
 
@@ -21,9 +23,10 @@ class ServiceNameTest {
     assertArrayEquals(new String[]{"test1", "test2"}, names);
   }
 
-  @Test
-  void excludeUninterestedServices() {
-    final var names = ServiceName.fromAll(Set.of("consul", "test1", "test1-sidecar-proxy", "test2"));
+  @ParameterizedTest
+  @ValueSource(strings = {"consul", "test1-sidecar-proxy"})
+  void excludeUninterestedServices(String serviceName) {
+    final var names = ServiceName.fromAll(Set.of("test1", serviceName, "test2"));
     assertArrayEquals(new String[]{"test1", "test2"}, names);
   }
 }
