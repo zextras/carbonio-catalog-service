7065184cce07ad516865f54921fea6c817423132
Davide Frison <davide.frison@zextras.com>
2025-08-05 14:21:04 +0200

    chore: add license header to java files

--- src/main/java/com/zextras/carbonio/catalog/app/health/HealthResource.java
+++ src/main/java/com/zextras/carbonio/catalog/app/health/HealthResource.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 /**
  * Health check logic follows the technical_specifications/health-check.md guideline provide by organization dev guide.
  */
