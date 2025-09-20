4657cd90445310fa505b09374da4ad89ce447a27
Marco Pernigo <marco.pernigo.07@gmail.com>
2025-09-20 16:28:55 +0200

    chore(ci): streamline jenkins pipeline with jenkins-packages-library
    * Consolidate build and upload stages for clarity
    * Remove deprecated package building logic
    * Update PKGBUILD to include quarkus package and correct checksums
    * Update build-package script
    
    Refs: IN-930 IN-908

--- build_packages.sh
+++ build_packages.sh
@@ -6,45 +6,11 @@
 #
 
 OS=${1:-"ubuntu-jammy"}
-
-if [[ -z $OS ]]
-then
-  echo "Please provide an OS as argument: (ubuntu-jammy, rocky-8)"
-  exit 1
-fi
-
-echo "Building for OS: $OS"
-
-if [[ $OS == "ubuntu-jammy" ]]
-then
-  docker run -it --rm \
-    --entrypoint=yap \
-    -v $(pwd)/artifacts/ubuntu-jammy:/artifacts \
-    -v $(pwd):/tmp/staging \
-    docker.io/m0rf30/yap-ubuntu-jammy:1.8 \
-    build ubuntu-jammy /tmp/staging/
-elif [[ $OS == "ubuntu-focal" ]]
-then
-  docker run -it --rm \
-    --entrypoint=yap \
-    -v $(pwd)/artifacts/ubuntu-focal:/artifacts \
-    -v $(pwd):/tmp/staging \
-    docker.io/m0rf30/yap-ubuntu-focal:1.8 \
-    build ubuntu-focal /tmp/staging/
-elif [[ $OS == "ubuntu-noble" ]]
-then
-  docker run -it --rm \
-    --entrypoint=yap \
-    -v $(pwd)/artifacts/ubuntu-noble:/artifacts \
-    -v $(pwd):/tmp/staging \
-    docker.io/m0rf30/yap-ubuntu-noble:1.8 \
-    build ubuntu-noble /tmp/staging/
-elif [[ $OS == "rocky-8" ]]
-then
-  docker run -it --rm \
-    --entrypoint=yap \
-    -v $(pwd)/artifacts/rocky-8:/artifacts \
-    -v $(pwd):/tmp/staging \
-    docker.io/m0rf30/yap-rocky-8:1.10 \
-    build rocky-8 /tmp/staging/
-fi
+tar czf package/carbonio-catalog-quarkus.tar.gz -C target/ quarkus-app
+docker run -it \
+  --entrypoint=yap \
+  -v $(pwd)/artifacts:/artifacts \
+  -v $(pwd):/project \
+  -w /project \
+  "docker.io/m0rf30/yap-${OS}:1.44" \
+  build "${OS}" .
